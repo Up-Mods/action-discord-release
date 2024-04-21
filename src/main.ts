4d36e1cd6f4d7794c7cea6a8576c56c728ba151b
Up <upcraftedlp@gmail.com>
2024-04-22 00:38:58 +0200

    initial files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+import {
+  EmbedBuilder,
+  WebhookClient,
+  WebhookMessageCreateOptions,
+  roleMention
+} from 'discord.js'
 
 /**
  * The main function for the action.
@@ -7,18 +13,129 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const webhookUrl = core.getInput('webhook-url', { required: true })
+    const webhook = new WebhookClient({ url: webhookUrl })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // the following parts have been copied from the checkout action
+    // - https://github.com/actions/checkout/blob/1d96c772d19495a3b5c517cd2bc0cb401ea0529f/src/input-helper.ts#L20-L72
+    const qualifiedRepository =
+      core.getInput('repository') ||
+      `${github.context.repo.owner}/${github.context.repo.repo}`
+    core.debug(`qualified repository = '${qualifiedRepository}'`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const isWorkflowRepo =
+      qualifiedRepository.toUpperCase() ===
+      `${github.context.repo.owner}/${github.context.repo.repo}`.toUpperCase()
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const splitRepository = qualifiedRepository.split('/')
+    if (
+      splitRepository.length !== 2 ||
+      !splitRepository[0] ||
+      !splitRepository[1]
+    ) {
+      throw new Error(
+        `Invalid repository '${qualifiedRepository}'. Expected format {owner}/{repo}.`
+      )
+    }
+    const repositoryOwner = splitRepository[0]
+    const repositoryName = splitRepository[1]
+    core.debug(`repository owner = '${repositoryOwner}'`)
+    core.debug(`repository name = '${repositoryName}'`)
+
+    const projectName =
+      core.getInput('project-name', { required: false }) || isWorkflowRepo
+        ? repositoryName
+        : undefined
+    if (!projectName) {
+      throw new Error(
+        'project-name is required if the provided repository is not repository running the workflow!'
+      )
+    }
+
+    let version = core.getInput('version', { required: false })
+    if (!version && isWorkflowRepo) {
+      const ref = github.context.ref?.replace('refs/tags/', '')
+      if (ref) {
+        version = /v\d/.test(ref) ? ref.substring(1) : ref
+      }
+    }
+
+    const modrinthId = core.getInput('modrinth-project-id', { required: false })
+    const curseforgeId = core.getInput('curseforge-project-id', {
+      required: false
+    })
+
+    const thumbnailUrl = core.getInput('thumbnail-url', { required: false })
+
+    const notificationRole = core.getInput('notification-role-id', {
+      required: false
+    })
+
+    const description: string[] = [`# ${projectName} ${version}`, '']
+
+    if (modrinthId || curseforgeId) {
+      description.push('## Downloads:')
+      let downloads = ''
+      if (curseforgeId) {
+        if (downloads.length > 0) {
+          downloads += ' | '
+        }
+        downloads += `<:curseforge:1231714919561429023> [Curseforge](https://www.curseforge.com/projects/${curseforgeId})`
+      }
+      if (modrinthId) {
+        if (downloads.length > 0) {
+          downloads += ' | '
+        }
+        downloads += `<:modrinth:1231714923503943710> [Modrinth](https://modrinth.com/mod/${modrinthId})`
+      }
+      description.push(downloads)
+    }
+
+    description.push('')
+    description.push(
+      `<:github:1231714921331425310> [Source Code](https://github.com/${qualifiedRepository})`
+    )
+
+    let embed = new EmbedBuilder()
+      .setColor('#8dcf88')
+      // .setAuthor({
+      //   name: 'Up-Mods',
+      //   // profile icon of github org
+      //   iconURL: 'https://avatars.githubusercontent.com/u/141473891?s=64',
+      //   url: 'https://github.com/Up-Mods'
+      // })
+      .setTimestamp(new Date())
+      .setDescription(description.join('\n'))
+
+    if (thumbnailUrl) {
+      embed = embed.setThumbnail(thumbnailUrl)
+    }
+
+    const avatar = {
+      username: 'Mod Updates',
+      avatarURL: 'https://avatars.githubusercontent.com/u/141473891?s=256'
+    }
+    // TODO this cannot currently be crossposted; for that we would need a real bot, not just a webhook :(
+    const payload: WebhookMessageCreateOptions = {
+      ...avatar,
+      embeds: [embed]
+    }
+
+    const result = await webhook.send(payload)
+    core.debug(`Sent message with id ${result.id}`)
+    core.setOutput('message', result)
+
+    // ping the announcements role
+    if (notificationRole) {
+      core.debug(`Pinging role ${notificationRole}`)
+      await webhook.send({
+        ...avatar,
+        content: roleMention(notificationRole),
+        allowedMentions: {
+          roles: [notificationRole]
+        }
+      })
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
